f779cfcd8dee72d27d4eaa6e088d2d9b779974ed
Mahesh Nama <you@example.com>
2019-12-11 16:47:30 +0530

    Updates

---
+++ precache-manifest.a7fd81798cb796ca220c656ac6135b85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "8ab5bb4246f9b4a9fe78",
+    "url": "/tic-tac-toe/static/js/main.8ab5bb42.chunk.js"
+  },
+  {
+    "revision": "ed1a98a028dd73953538",
+    "url": "/tic-tac-toe/static/js/1.ed1a98a0.chunk.js"
+  },
+  {
+    "revision": "8ab5bb4246f9b4a9fe78",
+    "url": "/tic-tac-toe/static/css/main.a22f98ec.chunk.css"
+  },
+  {
+    "revision": "5f4d4c0e8679eed17084cc3f9550e7e2",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
